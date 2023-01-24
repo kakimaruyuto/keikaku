4fe4a5f2d93d66e52b16796e3aa613bcb6cb10f8
y_kakimaru <108059698+kakimaruyuto@users.noreply.github.com>
2023-01-24 23:03:06 +0900

    Add files via upload

--- 05_PHP_Basic/PHP_5-3.php
+++ 05_PHP_Basic/PHP_5-3.php
@@ -1,5 +1,21 @@
 <?php
 // 配列かどうか判定する組み込み関数を使用し、配列の場合は OK、配列でない場合は NG と表示する処理を作成してください。
 // null の場合の対策も行ってください。
+function limite($time){
+    return $time * 60;
+};
+
+if(is_array($time)){
+    echo 'OK' ;
+}else {
+    echo 'NG' ;
+}
+
+//nullの場合
+if(is_null($time)){
+    echo 'true';
+}else{
+    echo 'false';
+}
 
 ?>
